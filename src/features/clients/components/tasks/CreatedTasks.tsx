f2116b9ae840540371f3cf01b41c3c71abe548a7
Da-cruz <wilfrieddacruz@arolitec.com>
2023-07-29 19:11:59 +0000

    add table to component

--- src/features/clients/components/tasks/CreatedTasks.tsx
+++ src/features/clients/components/tasks/CreatedTasks.tsx
@@ -1,7 +1,30 @@
+import { Table } from "flowbite-react"
 import React from "react"
 
 const CreatedTasks = () => {
-  return <div>CreatedTasks</div>
+  return (
+    <React.Fragment>
+      <div>
+        <h1 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white mb-5">Tâches créees</h1>
+      </div>
+      <Table hoverable>
+        <Table.Head>
+          <Table.HeadCell>Titre</Table.HeadCell>
+          <Table.HeadCell>Descrption</Table.HeadCell>
+          <Table.HeadCell>Statut</Table.HeadCell>
+          <Table.HeadCell>Deadline</Table.HeadCell>
+          <Table.HeadCell>Assigné à</Table.HeadCell>
+          <Table.HeadCell>Date de création</Table.HeadCell>
+          <Table.HeadCell>Action</Table.HeadCell>
+        </Table.Head>
+        <Table.Body className="divide-y">
+          <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800"></Table.Row>
+          <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800"></Table.Row>
+          <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800"></Table.Row>
+        </Table.Body>
+      </Table>
+    </React.Fragment>
+  )
 }
 
 export default CreatedTasks
--- src/features/clients/components/users/TeamUsers.tsx
+++ src/features/clients/components/users/TeamUsers.tsx
@@ -1,7 +1,29 @@
+import { Table } from "flowbite-react"
 import React from "react"
 
 const TeamUsers = () => {
-  return <div>TeamUsers</div>
+  return (
+    <React.Fragment>
+      <div>
+        <h1 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white mb-5">Membres de l'équipe</h1>
+      </div>
+      <Table hoverable>
+        <Table.Head>
+          <Table.HeadCell>Nom d'utilisateur</Table.HeadCell>
+          <Table.HeadCell>Email</Table.HeadCell>
+          <Table.HeadCell>Rôle</Table.HeadCell>
+          <Table.HeadCell>Tâches créees</Table.HeadCell>
+          <Table.HeadCell>Tâches assignées</Table.HeadCell>
+          <Table.HeadCell>Action</Table.HeadCell>
+        </Table.Head>
+        <Table.Body className="divide-y">
+          <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800"></Table.Row>
+          <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800"></Table.Row>
+          <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800"></Table.Row>
+        </Table.Body>
+      </Table>
+    </React.Fragment>
+  )
 }
 
 export default TeamUsers
