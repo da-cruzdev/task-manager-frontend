e2e2c792faf1699724f85ea100f34c159c2ec42c
Da-cruz <wilfrieddacruz@arolitec.com>
2023-08-13 19:16:07 +0000

    add date and hour rendering to navbar

--- src/features/clients/components/cards/createdTask-card.tsx
+++ src/features/clients/components/cards/createdTask-card.tsx
@@ -8,25 +8,13 @@ import { Tasks } from "../../interfaces/tasks.interfaces"
 import { Link } from "react-router-dom"
 import { List, ListItem, ListItemPrefix, ListItemSuffix, Chip, Card, IconButton, Button } from "@material-tailwind/react"
 
-// function TrashIcon() {
-//   return (
-//     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="h-6 w-6">
-//       <path
-//         fillRule="evenodd"
-//         d="M16.5 4.478v.227a48.816 48.816 0 013.878.512.75.75 0 11-.256 1.478l-.209-.035-1.005 13.07a3 3 0 01-2.991 2.77H8.084a3 3 0 01-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 01-.256-1.478A48.567 48.567 0 017.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 013.369 0c1.603.051 2.815 1.387 2.815 2.951zm-6.136-1.452a51.196 51.196 0 013.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 00-6 0v-.113c0-.794.609-1.428 1.364-1.452zm-.355 5.945a.75.75 0 10-1.5.058l.347 9a.75.75 0 101.499-.058l-.346-9zm5.48.058a.75.75 0 10-1.498-.058l-.347 9a.75.75 0 001.5.058l.345-9z"
-//         clipRule="evenodd"
-//       />
-//     </svg>
-//   )
-// }
-
 function ClipboardIcon() {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
       <path
-        fill-rule="evenodd"
+        fillRule="evenodd"
         d="M10.5 3A1.501 1.501 0 009 4.5h6A1.5 1.5 0 0013.5 3h-3zm-2.693.178A3 3 0 0110.5 1.5h3a3 3 0 012.694 1.678c.497.042.992.092 1.486.15 1.497.173 2.57 1.46 2.57 2.929V19.5a3 3 0 01-3 3H6.75a3 3 0 01-3-3V6.257c0-1.47 1.073-2.756 2.57-2.93.493-.057.989-.107 1.487-.15z"
-        clip-rule="evenodd"
+        clipRule="evenodd"
       />
     </svg>
   )
--- src/features/clients/components/navbar/Navbar.tsx
+++ src/features/clients/components/navbar/Navbar.tsx
@@ -9,8 +9,11 @@ import { useNavigate } from "react-router-dom"
 import client from "../../../../app/graphql"
 import { UpdateUserInfoModal } from "../modals/UpdateUserInfoModal"
 import { UpdateUserData } from "../../interfaces/users.interfaces"
-import { Navbar, Typography, Button, Menu, MenuHandler, MenuList, MenuItem, Avatar } from "@material-tailwind/react"
+import { Navbar, Typography, Button, Menu, MenuHandler, MenuList, MenuItem, Avatar, Chip, IconButton } from "@material-tailwind/react"
 import { UserCircleIcon, ChevronDownIcon, Cog6ToothIcon, PowerIcon } from "@heroicons/react/24/outline"
+import { format } from "date-fns"
+import fr from "date-fns/locale/fr"
+import { BellIcon } from "@heroicons/react/24/solid"
 
 type UserProps = {
   user: User | null
@@ -23,7 +26,7 @@ export const ProfileMenu: React.FC<UserProps> = ({ user, handleLogout, handleUpd
 
   return (
     <>
-      <div className="lg:ml-auto">
+      <div>
         <Typography variant="h6">{user?.username || "Nom d'utilisateur inconnu"}</Typography>
         <Typography variant="small" color="gray" className="font-normal">
           {user?.email || "Email inconnu"}
@@ -72,8 +75,34 @@ export const ProfileMenu: React.FC<UserProps> = ({ user, handleLogout, handleUpd
   )
 }
 
+function Calendar() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z"
+      />
+    </svg>
+  )
+}
+
+function Clock() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5">
+      <path
+        fillRule="evenodd"
+        d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zM12.75 6a.75.75 0 00-1.5 0v6c0 .414.336.75.75.75h4.5a.75.75 0 000-1.5h-3.75V6z"
+        clipRule="evenodd"
+      />
+    </svg>
+  )
+}
+
 export default function ComplexNavbar() {
   const [userData, setUserData] = useState<User | null>(null)
+  const [currentDate, setCurrentDate] = useState<string>("")
+  const [currentTime, setCurrentTime] = useState<string>("")
 
   const dispatch: AppDispatch = useAppDispatch()
   const navigate = useNavigate()
@@ -91,6 +120,18 @@ export default function ComplexNavbar() {
     }
 
     fetchUser()
+
+    const now = new Date()
+    const formattedDate = format(now, `EEEE, d MMMM yyyy`, { locale: fr })
+    setCurrentDate(formattedDate)
+
+    const interval = setInterval(() => {
+      const now = new Date()
+      const formattedTime = format(now, "HH:mm:ss")
+      setCurrentTime(formattedTime)
+    }, 1000)
+
+    return () => clearInterval(interval)
   }, [dispatch])
 
   const onUpdateUser = (data: UpdateUserData) => {
@@ -122,6 +163,13 @@ export default function ComplexNavbar() {
     <Navbar className=" mx-auto max-w-full-xl p-2 lg lg:pl-6">
       <div className="relative mx-auto flex items-center text-blue-gray-900">
         <Typography className="mr-4 ml-2 cursor-pointer py-1.5 font-medium">Tableau de bord</Typography>
+        <div className="absolute top-2/4 left-1/4 hidden -translate-x-2/4 -translate-y-2/4 lg:flex ">
+          <Chip value={currentDate} variant="ghost" size="lg" className="cursor-pointer" icon={<Calendar />} />
+          <Chip value={currentTime} variant="outlined" size="sm" className="cursor-pointer ml-5 text-md" icon={<Clock />} />
+        </div>
+        <IconButton variant="text" className=" lg:ml-auto mr-3">
+          <BellIcon className="h-5 w-5" color="black" />
+        </IconButton>
         <ProfileMenu user={userData} handleLogout={onLogout} handleUpdateSubmit={onUpdateUser} />
       </div>
     </Navbar>
